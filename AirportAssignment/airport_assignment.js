9af5d0524e2ba3d8f133218e49ed681c8367b90b
HKUMAS <hasan.kumas@capgemini.com>
2020-04-26 19:34:49 +0200

    assignment day_3

--- AirportAssignment/airport_assignment.js
+++ AirportAssignment/airport_assignment.js
@@ -1,25 +1,20 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Airplane = /** @class */ (function () {
     function Airplane(planeIdentification) {
-        this.maxNumOfPassengers = 100;
-        this.currentNumOfPassengers = 0;
         this.planeIdentification = planeIdentification;
     }
-    Airplane.prototype.loadPassengers = function (numOfPassengers) {
-        if (this.currentNumOfPassengers + numOfPassengers <= this.maxNumOfPassengers) {
-            this.currentNumOfPassengers += numOfPassengers;
-            console.log("Airplane " + this.planeIdentification + " loads " + numOfPassengers + " passengers.");
-        }
-        else {
-            var loaded = this.maxNumOfPassengers - this.currentNumOfPassengers;
-            var unloaded = numOfPassengers - loaded;
-            console.log("Airplane " + this.planeIdentification + " charges " + loaded + " passengers, " + unloaded + " do not fit.");
-            this.currentNumOfPassengers = this.maxNumOfPassengers;
-        }
-    };
-    Airplane.prototype.unloadPassengers = function () {
-        console.log("Airplane " + this.planeIdentification + " discharges " + this.currentNumOfPassengers + " passengers.");
-        this.currentNumOfPassengers = 0;
-    };
     Airplane.prototype.takeOff = function () {
         if (this.isCurrentlyFlying) {
             console.log("Airplane " + this.planeIdentification + " can not take off, because we are already flying.");
@@ -38,7 +33,34 @@ var Airplane = /** @class */ (function () {
             this.isCurrentlyFlying = false;
         }
     };
-    Airplane.prototype.hasRooms = function () {
+    return Airplane;
+}());
+var PeoplePlane = /** @class */ (function (_super) {
+    __extends(PeoplePlane, _super);
+    function PeoplePlane(planeIdentification) {
+        var _this = _super.call(this, planeIdentification) || this;
+        _this.maxNumOfPassengers = 100;
+        _this.currentNumOfPassengers = 0;
+        _this.type = AirplaneType.people;
+        return _this;
+    }
+    PeoplePlane.prototype.load = function (numOfPassengers) {
+        if (this.currentNumOfPassengers + numOfPassengers <= this.maxNumOfPassengers) {
+            this.currentNumOfPassengers += numOfPassengers;
+            console.log("Airplane " + this.planeIdentification + " loads " + numOfPassengers + " passengers.");
+        }
+        else {
+            var loaded = this.maxNumOfPassengers - this.currentNumOfPassengers;
+            var unloaded = numOfPassengers - loaded;
+            console.log("Airplane " + this.planeIdentification + " charges " + loaded + " passengers, " + unloaded + " do not fit.");
+            this.currentNumOfPassengers = this.maxNumOfPassengers;
+        }
+    };
+    PeoplePlane.prototype.unload = function () {
+        console.log("Airplane " + this.planeIdentification + " discharges " + this.currentNumOfPassengers + " passengers.");
+        this.currentNumOfPassengers = 0;
+    };
+    PeoplePlane.prototype.hasRooms = function () {
         if (this.currentNumOfPassengers < this.maxNumOfPassengers) {
             return true;
         }
@@ -46,53 +68,127 @@ var Airplane = /** @class */ (function () {
             return false;
         }
     };
-    Airplane.prototype.getRooms = function () {
+    PeoplePlane.prototype.getRooms = function () {
         return this.maxNumOfPassengers - this.currentNumOfPassengers;
     };
-    return Airplane;
-}());
+    return PeoplePlane;
+}(Airplane));
+var CargoPlane = /** @class */ (function (_super) {
+    __extends(CargoPlane, _super);
+    function CargoPlane(planeIdentification) {
+        var _this = _super.call(this, planeIdentification) || this;
+        _this.maxAmountOfCargo = 20;
+        _this.currentCargo = 0;
+        _this.type = AirplaneType.cargo;
+        return _this;
+    }
+    CargoPlane.prototype.load = function (amountOfCargo) {
+        if (this.currentCargo + amountOfCargo <= this.maxAmountOfCargo) {
+            this.currentCargo += amountOfCargo;
+            console.log("Airplane " + this.planeIdentification + "loads " + amountOfCargo + " tons cargo.");
+        }
+        else {
+            var loaded = this.maxAmountOfCargo - this.currentCargo;
+            var unloaded = amountOfCargo - loaded;
+            console.log("Airplane " + this.planeIdentification + " loads " + loaded + " tons of cargo, " + unloaded + " tons do not fit.");
+            this.currentCargo = this.maxAmountOfCargo;
+        }
+    };
+    CargoPlane.prototype.unload = function () {
+        console.log("Airplane " + this.planeIdentification + " discharges " + this.currentCargo + " tons of cargo.");
+        this.currentCargo = 0;
+    };
+    CargoPlane.prototype.hasRooms = function () {
+        if (this.currentCargo < this.maxAmountOfCargo) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    CargoPlane.prototype.getRooms = function () {
+        return this.maxAmountOfCargo - this.currentCargo;
+    };
+    return CargoPlane;
+}(Airplane));
+var AirplaneType;
+(function (AirplaneType) {
+    AirplaneType[AirplaneType["people"] = 0] = "people";
+    AirplaneType[AirplaneType["cargo"] = 1] = "cargo";
+})(AirplaneType || (AirplaneType = {}));
 var Airport = /** @class */ (function () {
     function Airport(airportName) {
         this.airplanes = [];
         this.airportName = airportName;
-        var airplane1 = new Airplane("ABC123");
-        var airplane2 = new Airplane("DDD888");
-        var airplane3 = new Airplane("ODL345");
+        var airplane1 = new PeoplePlane("ABC123");
+        var airplane2 = new PeoplePlane("DDD888");
+        var airplane3 = new PeoplePlane("ODL345");
+        var airplane4 = new CargoPlane(("FF2134"));
+        var airplane5 = new CargoPlane(("PLA166"));
         this.airplanes[0] = airplane1;
         this.airplanes[1] = airplane2;
         this.airplanes[2] = airplane3;
+        this.airplanes[3] = airplane4;
+        this.airplanes[4] = airplane5;
     }
-    Airport.prototype.printAll = function () {
-        console.log("Aircraft from airport " + this.airportName);
+    Airport.prototype.toString = function () {
+        console.log("Aircrafts from airport " + this.airportName);
+        console.log("Passenger planes from airport " + this.airportName);
         for (var _i = 0, _a = this.airplanes; _i < _a.length; _i++) {
             var airplane = _a[_i];
-            console.log("Airplane " + airplane.planeIdentification);
-        }
-    };
-    Airport.prototype.requestPlane = function () {
-        for (var _i = 0, _a = this.airplanes; _i < _a.length; _i++) {
-            var aplane = _a[_i];
-            if (!aplane.isCurrentlyFlying && aplane.hasRooms()) {
-                console.log("Plane " + aplane.planeIdentification + " requested. Is not flying, still room for " + aplane.getRooms() + " passengers.");
-                return aplane;
+            if (airplane.type === AirplaneType.people) {
+                console.log("Passenger plane " + airplane.planeIdentification);
             }
         }
-        return null;
+        console.log("Cargo planes from airport " + this.airportName);
+        for (var _b = 0, _c = this.airplanes; _b < _c.length; _b++) {
+            var airplane = _c[_b];
+            if (airplane.type === AirplaneType.cargo) {
+                console.log("Cargo plane " + airplane.planeIdentification);
+            }
+        }
+    };
+    Airport.prototype.requestPlane = function (type, loadingAmount) {
+        switch (type) {
+            case AirplaneType.people:
+                var isEmpty = true;
+                for (var _i = 0, _a = this.airplanes; _i < _a.length; _i++) {
+                    var aplane = _a[_i];
+                    if (aplane.type === AirplaneType.people && !aplane.isCurrentlyFlying && aplane.hasRooms()) {
+                        console.log("Plane " + aplane.planeIdentification + " requested. Is not flying, still room for " + aplane.getRooms() + " passengers.");
+                        aplane.load(loadingAmount);
+                        isEmpty = false;
+                        break;
+                    }
+                }
+                if (isEmpty) {
+                    console.log("There is no available passenger plane.");
+                }
+                break;
+            case AirplaneType.cargo:
+                isEmpty = true;
+                for (var _b = 0, _c = this.airplanes; _b < _c.length; _b++) {
+                    var aplane2 = _c[_b];
+                    if (aplane.type === AirplaneType.cargo && !aplane2.isCurrentlyFlying && aplane2.hasRooms()) {
+                        console.log("Plane " + aplane2.planeIdentification + " requested. Is not flying, still room for " + aplane2.getRooms() + " tons cargo.");
+                        aplane2.load(loadingAmount);
+                        isEmpty = false;
+                        break;
+                    }
+                }
+                if (isEmpty) {
+                    console.log("There is no available cargo plane.");
+                }
+                break;
+        }
     };
     return Airport;
 }());
-var airplane1 = new Airplane('ABC123');
-var airplane2 = new Airplane('DDD888');
-airplane1.loadPassengers(140);
-airplane1.land();
-airplane1.takeOff();
-airplane1.land();
-airplane1.unloadPassengers();
-airplane2.loadPassengers(160);
-airplane2.takeOff();
-airplane2.takeOff();
-airplane2.land();
-airplane2.unloadPassengers();
+//This part is for implemantation
 var airport = new Airport('Eindhoven');
-airport.printAll();
-airport.requestPlane();
+airport.requestPlane(AirplaneType.people, 40);
+airport.toString();
+airport.airplanes[0].load(40);
+airport.airplanes[0].takeOff();
+airport.airplanes[0].land();
+airport.airplanes[0].unload();
--- AirportAssignment/airport_assignment.ts
+++ AirportAssignment/airport_assignment.ts
@@ -1,31 +1,17 @@
-class Airplane {
+abstract class Airplane {
     public planeIdentification: string;
-    public maxNumOfPassengers: number = 100;
-    public currentNumOfPassengers: number = 0;
     public isCurrentlyFlying: boolean;
     public cruiseSpeed: number;
+    public type: AirplaneType;
 
     constructor(planeIdentification: string){
         this.planeIdentification = planeIdentification;
     }
 
-    public loadPassengers(numOfPassengers: number): void {
-        if (this.currentNumOfPassengers + numOfPassengers <= this.maxNumOfPassengers) {
-            this.currentNumOfPassengers += numOfPassengers;
-            console.log("Airplane " + this.planeIdentification + " loads " + numOfPassengers + " passengers.");
-        }
-        else{
-            const loaded: number = this.maxNumOfPassengers-this.currentNumOfPassengers;
-            const unloaded: number = numOfPassengers-loaded;
-            console.log("Airplane "+ this.planeIdentification+ " charges " + loaded+ " passengers, "+ unloaded+ " do not fit.");
-            this.currentNumOfPassengers= this.maxNumOfPassengers;
-        }
-    }
-
-    public unloadPassengers(): void {
-        console.log("Airplane " + this.planeIdentification + " discharges " + this.currentNumOfPassengers + " passengers.");
-        this.currentNumOfPassengers=0;
-    }
+    public abstract load(numOfPassengers: number): void;
+    public abstract unload(): void;
+    public abstract getRooms(): number;
+    public abstract hasRooms(): boolean;
 
     public takeOff(): void {
         if(this.isCurrentlyFlying){
@@ -46,6 +32,35 @@ class Airplane {
             this.isCurrentlyFlying = false;
         }
     }
+}
+
+class PeoplePlane extends Airplane{
+    public maxNumOfPassengers: number = 100;
+    public currentNumOfPassengers: number = 0;
+    
+    constructor(planeIdentification: string){
+        super(planeIdentification);
+        this.type = AirplaneType.people;
+    }
+
+    public load(numOfPassengers: number): void {
+        if (this.currentNumOfPassengers + numOfPassengers <= this.maxNumOfPassengers) {
+            this.currentNumOfPassengers += numOfPassengers;
+            console.log("Airplane " + this.planeIdentification + " loads " + numOfPassengers + " passengers.");
+        }
+        else{
+            const loaded: number = this.maxNumOfPassengers-this.currentNumOfPassengers;
+            const unloaded: number = numOfPassengers-loaded;
+            console.log("Airplane "+ this.planeIdentification+ " charges " + loaded+ " passengers, "+ unloaded+ " do not fit.");
+            this.currentNumOfPassengers= this.maxNumOfPassengers;
+        }
+    }
+
+    public unload(): void {
+        console.log("Airplane " + this.planeIdentification + " discharges " + this.currentNumOfPassengers + " passengers.");
+        this.currentNumOfPassengers=0;
+    }
+
 
     public  hasRooms(): boolean{
         if(this.currentNumOfPassengers < this.maxNumOfPassengers){
@@ -60,53 +75,131 @@ class Airplane {
     }
 }
 
+class CargoPlane extends Airplane{    
+    public maxAmountOfCargo: number = 20;
+    public currentCargo: number = 0;
+
+    constructor(planeIdentification: string){
+        super(planeIdentification);
+        this.type = AirplaneType.cargo;        
+    }
+
+    public load(amountOfCargo: number): void {
+
+        if (this.currentCargo + amountOfCargo <= this.maxAmountOfCargo) {
+            this.currentCargo += amountOfCargo;
+            console.log("Airplane " + this.planeIdentification + "loads " + amountOfCargo + " tons cargo.");
+        }
+        else{
+            const loaded: number= this.maxAmountOfCargo-this.currentCargo;
+            const unloaded: number= amountOfCargo -loaded;
+            console.log("Airplane "+ this.planeIdentification+ " loads " + loaded+ " tons of cargo, "+ unloaded+ " tons do not fit.");
+            this.currentCargo= this.maxAmountOfCargo;
+        }
+
+    }
+
+    public unload(): void {
+        console.log("Airplane " + this.planeIdentification + " discharges " + this.currentCargo + " tons of cargo.");
+        this.currentCargo=0;
+    }
+
+    public  hasRooms(): boolean{
+        if(this.currentCargo < this.maxAmountOfCargo){
+            return  true;
+        }else{
+            return  false;
+        }
+    }
+
+    public getRooms(): number{
+        return  this.maxAmountOfCargo-this.currentCargo;
+    }
+}
+
+enum AirplaneType {
+    people,
+    cargo
+}
+
 class Airport {
     public airportName: string;
     public airplanes: Airplane[] = [];
 
     constructor(airportName: string) {
         this.airportName = airportName;
-        const airplane1: Airplane = new Airplane("ABC123");
-        const airplane2: Airplane = new Airplane("DDD888");
-        const airplane3: Airplane = new Airplane("ODL345");
+        const airplane1: Airplane = new PeoplePlane("ABC123");
+        const airplane2: Airplane = new PeoplePlane("DDD888");
+        const airplane3: Airplane = new PeoplePlane("ODL345");
+        const airplane4: Airplane = new CargoPlane(("FF2134"));
+        const airplane5: Airplane = new CargoPlane(("PLA166"));
         this.airplanes[0] = airplane1;
         this.airplanes[1] = airplane2;
         this.airplanes[2] = airplane3;
+        this.airplanes[3] = airplane4;
+        this.airplanes[4] = airplane5;
     }
 
-    public printAll(): void {
-        console.log("Aircraft from airport " + this.airportName);
+    public toString(): void {
+        console.log("Aircrafts from airport " + this.airportName);
+        console.log("Passenger planes from airport " + this.airportName);
         for (var airplane of this.airplanes) {
-            console.log("Airplane " + airplane.planeIdentification);
-        }
-    }
-
-    public requestPlane(): Airplane {
-        for (var aplane of this.airplanes) {
-            if (!aplane.isCurrentlyFlying && aplane.hasRooms()) {
-                console.log("Plane " + aplane.planeIdentification + " requested. Is not flying, still room for " + aplane.getRooms() + " passengers.");
-                return aplane;
+            if(airplane.type === AirplaneType.people) {
+                console.log("Passenger plane " + airplane.planeIdentification);
             }
         }
-    return  null;
+        console.log("Cargo planes from airport " + this.airportName);
+        for (var airplane of this.airplanes) {
+            if(airplane.type === AirplaneType.cargo) {
+                console.log("Cargo plane " + airplane.planeIdentification);
+            }
+        }
+    }
+
+    public requestPlane(type: AirplaneType, loadingAmount: number): void {        
+
+        switch (type) {
+            case AirplaneType.people:
+                let isEmpty: boolean =true;
+                for (var aplane of this.airplanes) {
+
+                    if (aplane.type === AirplaneType.people && !aplane.isCurrentlyFlying && aplane.hasRooms()) {
+                        console.log("Plane " + aplane.planeIdentification + " requested. Is not flying, still room for " + aplane.getRooms() + " passengers.");
+                        aplane.load(loadingAmount);
+                        isEmpty=false;
+                        break;
+                    }
+
+                }
+                if(isEmpty) {
+                    console.log("There is no available passenger plane.");
+                }
+                break;
+            case AirplaneType.cargo:
+                isEmpty=true;
+                for (var aplane2 of this.airplanes) {
+                    if (aplane.type === AirplaneType.cargo && !aplane2.isCurrentlyFlying && aplane2.hasRooms()) {
+                        console.log("Plane " + aplane2.planeIdentification + " requested. Is not flying, still room for " + aplane2.getRooms() + " tons cargo.");
+                        aplane2.load(loadingAmount);
+                        isEmpty=false;
+                        break;
+                    }
+                }
+                if(isEmpty) {
+                    console.log("There is no available cargo plane.");
+                }
+                break;
+        }
     }
 }
 
-const airplane1: Airplane = new Airplane('ABC123');
-const airplane2: Airplane = new Airplane('DDD888');
 
-airplane1.loadPassengers(140);
-airplane1.land();
-airplane1.takeOff();
-airplane1.land();
-airplane1.unloadPassengers();
-
-airplane2.loadPassengers(160);
-airplane2.takeOff();
-airplane2.takeOff();
-airplane2.land();
-airplane2.unloadPassengers();
-
+//This part is for implemantation
 const airport: Airport = new Airport('Eindhoven');
-airport.printAll();
-airport.requestPlane();
+
+airport.requestPlane(AirplaneType.people, 40);
+airport.toString();
+airport.airplanes[0].load(40);
+airport.airplanes[0].takeOff();
+airport.airplanes[0].land();
+airport.airplanes[0].unload();
